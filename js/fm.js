2fe09fb3097a87c8bac09c2359ace63f3548e804
sigaer <a1106865024@163.com>
2020-12-18 18:53:18 +0800

    demo

---
+++ js/fm.js
@@ -0,0 +1,112 @@
+var allA=document.getElementsByTagName("a");
+            window.onload=function(){
+                var url=window.location.href;
+            var flag=url.indexOf("flag");
+            var cookie=document.cookie;
+            if(cookie!=0){
+                var c=cookie.split(';')
+                var n=c[0].split('=')
+                var p=c[1].split('=')
+                if(n[1]!=''&&p[1]!='')
+                flag=1;
+                else
+                flag=-1;
+            }
+            var header=document.getElementsByClassName("header");
+            var index=4;
+            var h3=document.getElementsByTagName("h3");
+            if(flag!=-1)
+            {
+                nick=document.cookie.split(";")[0].split("=")[1];
+            password=document.cookie.split(";")[1].split("=")[1];
+            h3[1].innerHTML="欢迎"+nick+"访问本网站!";
+            allA[0].setAttribute("hidden","hidden");
+            allA[1].setAttribute("hidden","hidden");
+            allA[3].removeAttribute("hidden");
+            if(nick=="admin")
+            {
+                allA[2].removeAttribute("hidden");
+                h3[1].innerHTML="现在是开发状态";
+            }
+            }
+            else
+            {
+                h3[1].innerHTML="";
+            }
+            var round=1;
+            allA[index].style.backgroundColor="black";
+        var start=setInterval(autoleft,10);
+            for(var i=4;i<9;i++)
+            {
+                allA[i].num=i;
+                allA[i].onclick=function(){
+                    index=this.num;
+                    header[0].style.left=-960*(index-4)+"px";
+                    setA();
+                }
+            }
+
+            function setA()
+            {
+            for(var i=4;i<9;i++)
+            {
+                allA[i].style.backgroundColor="";
+            }
+            allA[index].style.backgroundColor="black";
+            round=(index-4)*320+1;
+            }
+        function leftmove()
+        {
+            start=setInterval(autoleft,10);
+        }
+        function autoleft()
+        {
+            header[0].style.left=-3*round+"px";
+            round++;
+            if((round-1)%320==0&&(round!=1))
+                {
+                    for(var i=4;i<9;i++)
+            {
+                allA[i].style.backgroundColor="";
+            }
+                    allA[(round-1)/320+4].style.backgroundColor="black";
+                    clearInterval(start);
+                    setTimeout(leftmove,4000);
+                }
+            if(round>=1281)
+            {
+                round=1;
+                setTimeout(function(){
+                    header[0].style.left=0;
+                    for(var i=4;i<9;i++)
+            {
+                allA[i].style.backgroundColor="";
+            }
+                    allA[(round-1)/320+4].style.backgroundColor="black";
+                },2000);
+            }
+            }
+        
+            }
+        function aboutme(){
+            alert("这里是关于我的内容");
+        }
+        function qq(){
+            alert("1106865024");
+        }
+        function logout()
+        {
+            alert("您已退出登录！");
+            var date=new Date();
+                date.setTime(date.getTime()-10000);
+                document.cookie="name=; expire="+date.toGMTString()+"; path=/";
+                document.cookie="password=; expire="+date.toGMTString()+"; path=/";
+            var table=document.getElementById("login");
+            var logb=document.getElementById("log");
+            var logout=document.getElementById("logout");
+            var wel=document.getElementById("welcome");
+            allA[0].removeAttribute("hidden");
+            allA[1].removeAttribute("hidden");
+            allA[3].setAttribute("hidden","hidden");
+            window.location.href='p1.html'
+        }
---
+++ tpl/post.tpl
@@ -0,0 +1,107 @@
+ <div id="head">
+        <a href="login.html">登录</a>
+        <a href="register.html">注册</a>
+        <a href="note.html" hidden="hidden">待办事项</a>
+        <a href="#" hidden="hidden" onclick=logout()>登出</a>
+    </div>
+    <div id="outer">
+            <ul class="header">
+                <li><img src="head/6.jpg" alt=""></li>
+                <li><img src="head/41.jpg" alt=""></li>
+                <li><img src="head/46.jpg" alt=""></li>
+                <li><img src="head/68.jpg" alt=""></li>
+                <li><img src="head/80.jpg" alt=""></li>
+            </ul>
+            <div id="navDiv">
+                <a href="javascript:;"></a>
+                <a href="javascript:;"></a>
+                <a href="javascript:;"></a>
+                <a href="javascript:;"></a>
+                <a href="javascript:;"></a>
+            </div>
+    </div>
+    <div class="mid1">
+        <div class="logo">
+            <img src="miku/4.jpg" alt="0">
+        </div>
+        <div class="menu">
+            <ul><li><a href="#">首页</a></li>
+                <li><a href="javascript:void(0);" onclick="aboutme()">关于我</a></li>
+                <li><a href="music.html">音乐</a></li>
+                <li><a href="pic.html">相册</a></li>
+                <li><a href="#">心情</a></li>
+                <li><a href="#">留言</a></li></ul>
+        </div>
+            <div class="rtitle">
+                <div class="knowme">
+                    <h3>认识我</h3>
+                </div>
+                <div class="gz">
+                    <ul>
+                        <li><a href="#">新浪微博</a></li>
+                        <li><a href="#">腾讯微博</a></li>
+                    <li><a href="javascript:void(0);" onclick="qq()">QQ</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div id="wel">
+                <h3></h3>
+            </div>
+    </div>
+    
+    <div class="lis">
+        <div class=xtitle>
+            <h5>成员介绍</h5>
+        </div>
+        <ul><li><span class="sprite miku"></span><a href="intro.html">miku</a></li>
+            <li><span class="sprite rin"></span><a href="intro.html">rin</a></li>
+            <li><span class="sprite len"></span><a href="intro.html">len</a></li>
+            <li><span class="sprite luka"></span><a href="intro.html">luka</a></li>
+            <li><span class="sprite kaito"></span><a href="intro.html">kaito</a></li>
+            <li><span class="sprite meiko"></span><a href="intro.html">meiko</a></li>
+        </ul>
+    </div>
+    <div class=right>
+        <div class=xtitle>
+            <h5>站长推荐</h5>
+        </div>
+        <ul>
+            <li>
+                <a href="news.html?d=0">资讯1</a>
+            </li>
+            <li>
+                <a href="news.html?d=1">资讯2</a>
+            </li>
+            <li>
+                <a href="news.html?d=2">资讯3</a>
+            </li>
+            <li>
+                <a href="news.html?d=3">资讯4</a>
+            </li>
+            <li>
+                <a href="news.html?d=4">资讯5</a>
+            </li>
+            <li>
+                <a href="news.html?d=5">资讯6</a>
+            </li>
+            <li>
+                <a href="news.html?d=6">资讯7</a>
+            </li>
+            <li>
+                <a href="news.html?d=7">资讯8</a>
+            </li>
+            <li>
+                <a href="news.html?d=8">资讯9</a>
+            </li>
+            <li>
+                <a href="news.html?d=9">资讯10</a>
+            </li>
+        </ul>
+    </div>
+    <div class=center>
+        <div class=title>
+            <h4 id="tt">:title</h4>
+        </div>
+       <div class="news" id="c">
+       :content
+    </div>
